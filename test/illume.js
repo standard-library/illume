6750d84b5f9f06c026d67a6dc5afbe8f8af7ffcd
Jack Jennings <jack@standard-library.com>
2018-02-14 20:52:02 -0800

    Tests passing

--- test/illume.js
+++ test/illume.js
@@ -6,30 +6,44 @@ import { map, filter, reject, equals, uniq } from "ramda";
 import { query } from "@standard-library/q-prime";
 import { Kefir as K } from "kefir";
 
-test('should return active stream', function (t) {
-  document.body.innerHTML = "<div class='breakfasts' data-name='breakfasts' style='background-color:red;height:2000px;'>Breakfasts</div><div class='lunches' data-name='lunches' style='background-color:blue;height:1000px'>Lunches</div>";
-  window.scroll(10, 1000);
+test("should return active stream", function(t) {
   const sections = illume("name");
-  t.true(sections.active);
+
+  t.true("active" in sections);
   t.end();
 });
 
-test('should return inactive stream', function (t) {
-  document.body.innerHTML = "<div class='breakfasts' data-name='breakfasts' style='background-color:red;height:2000px;'>Breakfasts</div><div class='lunches' data-name='lunches' style='background-color:blue;height:1000px'>Lunches</div>";
-  window.scroll(10, 1000);
+test("should return inactive stream", function(t) {
   const sections = illume("name");
-  t.true(sections.inactive);
+
+  t.true("inactive" in sections);
   t.end();
 });
 
-test('should return active class', function (t) {
-  document.body.innerHTML = "<div class='breakfasts' data-name='breakfasts' style='background-color:red;height:2000px;'>Breakfasts</div><div class='lunches' data-name='lunches' style='background-color:blue;height:1000px'>Lunches</div>";
-  window.scroll(10, 2001);
+test("should return active class", function(t) {
+  document.body.innerHTML = `
+    <div class='breakfasts' data-name='breakfasts' style='background-color:red;height:2000px;'>
+      Breakfasts
+    </div>
+
+    <div class='lunches' data-name='lunches' style='background-color:blue;height:1000px'>
+      Lunches
+    </div>
+  `;
   const sections = illume("name");
-  sections.active.onValue((name) => {
-    const elements = document.querySelectorAll(`[data-name="${name}"]`);
-    const lunchElement = document.getElementsByClassName("lunches")[0];
-    t.equal(elements[0], lunchElement);
+
+  let values = [];
+  const stream = sections.active.observe(x => {
+    values.push(x);
+  });
+
+  window.scroll(0, 0);
+  window.scroll(0, 2100);
+
+  requestAnimationFrame(() => {
+    t.same(values, ["lunches"]);
     t.end();
+
+    stream.unsubscribe();
   });
 });
