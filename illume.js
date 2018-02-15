ff0a3e68a26332067966d011369349fda17e91c2
Jack Jennings <jack@standard-library.com>
2018-02-14 21:12:46 -0800

    Reformat with prettier

--- illume.js
+++ illume.js
@@ -7,8 +7,8 @@ const bottomAbove = (y, element) => {
   return y < offsetTop(element) + element.offsetHeight;
 };
 
-const offsetTop = (element) => {
-  if (!element.getClientRects().length ) {
+const offsetTop = element => {
+  if (!element.getClientRects().length) {
     return 0;
   }
 
@@ -16,10 +16,10 @@ const offsetTop = (element) => {
   const win = element.ownerDocument.defaultView;
 
   return rect.top + win.pageYOffset;
-}
+};
 
 function illume(attribute) {
-  const getName = (a) => a.dataset[attribute];
+  const getName = a => a.dataset[attribute];
   const areas = query(`[data-${attribute}]`);
   const names = areas.map(getName);
   const scroll = K.fromEvents(window, "scroll");
@@ -29,29 +29,30 @@ function illume(attribute) {
   const scrollY = redraw.map(() => window.scrollY);
   const windowHeight = redraw.map(() => window.innerHeight);
   const visibileY = K.combine([scrollY, windowHeight], (y, h) => y + h);
-  const viewedAreas = visibileY.map(function (y) {
+  const viewedAreas = visibileY.map(function(y) {
     return areas.filter(a => offsetAbove(y, a));
   });
 
-  const lastViewedArea = viewedAreas.map((as) => as[as.length - 1]);
-  const activeArea =
-    lastViewedArea.map(function (element) {
+  const lastViewedArea = viewedAreas.map(as => as[as.length - 1]);
+  const activeArea = lastViewedArea
+    .map(function(element) {
       if (element && bottomAbove(window.scrollY, element)) {
         return element;
       }
-    }).toProperty().skipDuplicates();
+    })
+    .toProperty()
+    .skipDuplicates();
 
   const active = activeArea.map(getName);
-  const inactive =
-    active
-      .map(a => names.filter(n => n !== a))
-      .map(uniq)
-      .flatten();
+  const inactive = active
+    .map(a => names.filter(n => n !== a))
+    .map(uniq)
+    .flatten();
 
   return {
     active,
     inactive
-  }
+  };
 }
 
 export default illume;
